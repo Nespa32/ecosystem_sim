2adbf0859da14d96da58f7bf27220d4ae92ef171
Ricardo <nespacontrib@gmail.com>
2016-11-24 23:41:10 +0000

    Move guideline rules to a seperate file

--- main.c
+++ main.c
@@ -3,53 +3,6 @@
 #include <string.h>
 #include <assert.h>
 
-/* Rabbit Rules */
-/*
-    Rabbits can move N/E/W/S, but not diagonally.
-    In each gen, rabbits attempt to move themselves to an empty adjacent cell. If
-     there are many empty adjacent cells, they choose one accordingly to a
-     common criteria for selecting adjacent cells (see below). If there is no
-     empty adjacent cell, they stay in the same place.
-    Rabbits can procreate whenever GEN_PROC_RABBITS generations have passed since
-     they were born or since they last procreated. Whenever a rabbit reaches such
-     age (to procreate) and makes a move, it leaves in its last position a new rabbit
-     and both rabbits' procreation age is set to zero.
-*/
-
-/* Fox Rules */
-/*
-    Foxes can move horizontally or vertically, but not diagonally.
-    In each generation, foxes try to eat a rabbit by moving to an adjacent cell
-     that is occupied by a rabbit. If there are many adjacent cells occupied with
-     rabbits, they choose one accordingly to the common criteria for selecting
-     adjacent cells (see below). If there is no adjacent cell occupied by a rabbit,
-     foxes attempt to move themselves to an empty adjacent cell by following the
-     same criteria. If there is no adjacent cell occupied with a rabbit or empty,
-     they stay in the same place.
-    Foxes starve and die whenever GEN_FOOD_FOXES generations have passed since
-     they were born or since they last ate a rabbit. Foxes die after not finding a
-     rabbit to eat and before attempting to move themselves to an empty adjacent cell.
-    Foxes can procreate whenever GEN_PROC_FOXES generations have passed since they
-     were born or since they last procreated. Whenever a fox reaches such age
-     (to procreate) and makes a move, it leaves in its last position a new fox and
-     both foxes' procreation age is set to zero.
-*/
-
-/* Rock Rules */
-/*
-    Rocks do not move and no animal can occupy its space.
-*/
-
-/* Criteria for Selecting Adjacent Cells */
-/*
-    By following the clockwise order, start numbering from 0 the possible P cells
-     to where a fox/rabbit can move (adjacent N/E/S/W cells).
-    Let G represent the current generation and (X,Y) represent the cell coordinates
-     where a fox/rabbit is positioned in the ecosystem space, then the adjacent
-     cell to be chosen is determined by (G+X+Y) mod P. Assume that the initial
-     generation is number 0 and that the world origin is (0,0).
-*/
-
 enum ObjectType
 {
     OBJECT_TYPE_NONE = 0,
